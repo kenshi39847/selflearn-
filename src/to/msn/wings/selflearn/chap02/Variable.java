e20c3a77c0a2ce44f5d6a2e3cc18ed7a3322ccdf
kenshi39847 <kenshi.t.blue@gmail.com>
2023-11-23 15:08:57 +0900

    変数の宣言2

--- src/to/msn/wings/selflearn/chap02/Variable.java
+++ src/to/msn/wings/selflearn/chap02/Variable.java
@@ -3,8 +3,17 @@ package to.msn.wings.selflearn.chap02;
 public class Variable {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		
 	}
-
 }
+
+/*
+  変数の宣言
+  データ型 変数名 [= 初期値] [,...]
+  データ型とは、変数に格納できる値の種類を表す情報である。
+  int data;
+  上記は「int（整数）値を入れるための、dataという名前の変数を準備する」という意味である。
+  また、変数を宣言する際にまとめて初期値を設定することができる。
+  int data1 = 108;
+  String message = "こんにちは、世界";
+*/
