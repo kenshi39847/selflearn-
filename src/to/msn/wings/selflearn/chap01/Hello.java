788c17db1839ae6da5068ef5e9f53dd0ec131dfd
kenshi39847 <kenshi.t.blue@gmail.com>
2023-11-23 12:26:50 +0900

    commit

--- src/to/msn/wings/selflearn/chap01/Hello.java
+++ src/to/msn/wings/selflearn/chap01/Hello.java
@@ -1,13 +1,13 @@
-package to.msn.wings.selflearn.chap01;
+package to.msn.wings.selflearn.chap01;  //パッケージ宣言
 
-import java.time.LocalDateTime;
+import java.time.LocalDateTime;  //import宣言
 
-public class Hello {
+public class Hello {  //クラス宣言（開始）
 
-	public static void main(String[] args) {
-		LocalDateTime time = LocalDateTime.now();
-		System.out.println(time);
+	public static void main(String[] args) {  //メソッド宣言（開始）
+		LocalDateTime time = LocalDateTime.now();  //命令文
+		System.out.println(time);  //命令文
 
-	}
+	}  //メソッド宣言（終了）
 
-}
+}  //クラス宣言（終了）
