66a23a66b72ec3bb430d3582ab7f99033932c375
kenshi39847 <kenshi.t.blue@gmail.com>
2023-11-23 16:13:38 +0900

    データ型の分類

---
+++ src/to/msn/wings/selflearn/chap02/DataType.java
@@ -0,0 +1,16 @@
+package to.msn.wings.selflearn.chap02;
+
+public class DataType {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
+/*
+  プログラムの中で扱うデータの種類のことを「データ型」という。
+  Javaでは文字列を入れるべき定数に数値を代入することができない（逆もある）という性質を「静的型付け」という。
+  データ型は大まかに「基本型」と「参照型」に分けられ、基本型と参照型とでは値の扱いが異なる。
+*/
+
