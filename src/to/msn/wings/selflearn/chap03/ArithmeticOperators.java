49d5deb7ce320a06396fd3129872bef96402eb87
kenshi39847 <kenshi.t.blue@gmail.com>
2023-11-24 18:06:27 +0900

    +演算子による文字列結合

--- src/to/msn/wings/selflearn/chap03/ArithmeticOperators.java
+++ src/to/msn/wings/selflearn/chap03/ArithmeticOperators.java
@@ -1,17 +1,34 @@
 package to.msn.wings.selflearn.chap03;
 
-import java.time.LocalDateTime;
-
 public class ArithmeticOperators {
 
 	public static void main(String[] args) {
-		System.out.println(1 + 2);
-	    System.out.println("a" + 5);
-	    System.out.println(5 + "b");
-	    System.out.println("1" + "2");
-	    System.out.println("a" + LocalDateTime.now());
+		//System.out.println(1 + 2);
+	    //System.out.println("a" + 5);
+	    //System.out.println(5 + "b");
+	    //System.out.println("1" + "2");
+	    //System.out.println("a" + LocalDateTime.now());
 	    // System.out.println(1 + LocalDateTime.now()); //エラー
-
+	    
+		/*
+		var start = System.currentTimeMillis();
+	    var result = "";
+	    for (var i = 0; i < 100000; i++) {
+	      result += "いろは";
+	    }
+	    System.out.println(result.length());
+	    var end = System.currentTimeMillis();
+	    System.out.println((end - start)  + "ミリ秒");
+        */
+		var start = System.currentTimeMillis();
+	    var builder = new StringBuilder();
+	    for (var i = 0; i < 100000; i++) {
+	      builder.append("いろは");
+	    }
+	    var result = builder.toString();
+	    System.out.println(result.length());
+	    var end = System.currentTimeMillis();
+	    System.out.println((end - start)  + "ミリ秒");
 	}
 
 }
@@ -20,7 +37,7 @@ public class ArithmeticOperators {
   与えられた変数やリテラルに対して、あらかじめ決められた処理を行うための記号を「演算子（オペレーター）」という。
   演算子によって処理される変数/リテラルのことを被演算子（オペランド）という。
   Javaの演算子には「算術演算子」、「代入演算子」、「関係演算子」、「論理演算子」、「ビット演算子」がある。
-*/
+
 
 /*
   「算術演算子（代数演算子）」は四則演算をはじめ、日常的な数字で利用する演算子を提供する。
