54bff64c9e2e34b828363055357e4dcd18ee527e
kenshi39847 <kenshi.t.blue@gmail.com>
2023-11-28 06:33:15 +0900

    while/do...while命令

---
+++ src/to/msn/wings/selflearn/chap04/IterativeProcessing.java
@@ -0,0 +1,50 @@
+package to.msn.wings.selflearn.chap04;
+
+public class IterativeProcessing {
+
+	public static void main(String[] args) {
+		/*
+		var i = 1;
+	    while (i < 6) {
+	      System.out.println(i + "番目のループです。");
+	      i++;
+	    }
+	    */
+		/*結果：1番目のループです。
+	            2番目のループです。
+	            3番目のループです。
+	            4番目のループです。
+	            5番目のループです。
+	    */
+		
+		/*
+		var i = 1;
+	    do {
+	      System.out.println(i + "番目のループです。");
+	      i++;
+	    } while (i < 6);
+	    */
+		/*結果：1番目のループです。
+	            2番目のループです。
+	            3番目のループです。
+	            4番目のループです。
+	            5番目のループです。
+	    */
+
+	}
+
+}
+
+/*
+  while命令
+  while(条件式) {
+    ...条件式がtrueである間、繰り返し実行する処理...
+  }
+*/
+
+/*
+  do...while命令
+  do {
+    ...条件式がtrueである間、繰り返し実行する処理...
+  } while(条件式);
+*/
