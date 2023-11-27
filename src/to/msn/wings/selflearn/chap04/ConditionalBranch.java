074acd5ab726ba6f48a7b108445f697275acec9c
kenshi39847 <kenshi.t.blue@gmail.com>
2023-11-27 20:32:25 +0900

    switch命令

--- src/to/msn/wings/selflearn/chap04/ConditionalBranch.java
+++ src/to/msn/wings/selflearn/chap04/ConditionalBranch.java
@@ -43,6 +43,38 @@ public class ConditionalBranch {
 	    else
 	      System.out.println("変数iは1ではありません。");  //結果：変数iは1です。
 	    
+	    var rank = "甲";
+
+	    switch (rank) {
+	      case "甲":
+	        System.out.println("大変良いです。");
+	        break;
+
+	      case "乙":
+	        System.out.println("良いです。");
+	        break;
+
+	      case "丙":
+	        System.out.println("がんばりましょう。");
+	        break;
+
+	      default:
+	        System.out.println("？？？");
+	        break;
+	    }  //結果：大変良いです。
+	    
+	    var drink = "ビール";
+	    switch (drink) {
+	      case "日本酒":
+	      case "ビール":
+	      case "ワイン":
+	        System.out.println("醸造酒です。");
+	        break;
+	      case "ブランデー":
+	      case "ウイスキー":
+	        System.out.println("蒸留酒です。");
+	        break;
+	    }  //結果：醸造酒です。
 
 	}
 
@@ -97,4 +129,43 @@ public class ConditionalBranch {
   if、else if、elseブロック配下の文が1つである場合、ブロック（{...}）は省略できる。
   しかし、{...}を省略してもそれほどコードは短くなるわけではなく、むしろブロックの範囲が不明確になるため
   バグの温床になりやすいためおすすめしない。
+*/
+
+/*
+  等価演算子による多岐分岐に特化した条件分岐命令を「switch命令」という。
+  同じような式を繰り返し記述する必要がなくなるので、コードが読みやすくなる。
+*/
+
+/*
+  switch命令
+  switch(式) {
+    case 値1:
+      ...「式 = 値1」の場合に実行する処理...
+      break;
+    case 値2:
+      ...「式 = 式2」の場合に実行する処理...
+      break;
+    ...
+    default:
+      ...すべての値に合致しない場合に実行する処理
+      break;
+  }
+*/
+
+/*
+  switch命令では次のような流れで実行すべき処理を決定する。
+  1.switchブロックの式を評価
+  2.1の値に合致するcase句を実行
+  3.対応するcase句が見つからない場合には、default句を実行
+*/
+
+/*
+  case/defaultはブロックではなく「xxxxx :」の形式で表された「句（ラベル）」である。
+  break命令のないcase/default句では、そのまま後続のcase/default句が実行されてしまう。
+*/
+
+/*
+  break命令を意図的に略して、複数のcase/default句を続けて実行していくことを「フォールスルー」という。
+  しかし、フォールスルーは一般的にコードを読みにくくする原因となるので、例外を除き避けるべきである。
+  その例外とは、文を挟まずに複数のcase句を列記する場合である。
 */
