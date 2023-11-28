dd7eb064c0b8e57f3c78b212c5856cde2911e304
kenshi39847 <kenshi.t.blue@gmail.com>
2023-11-29 08:59:15 +0900

    演算子の練習

---
+++ src/to/msn/wings/selflearn/chap03/Chap03Practice.java
@@ -0,0 +1,14 @@
+package to.msn.wings.selflearn.chap03;
+
+public class Chap03Practice {
+
+	public static void main(String[] args) {
+		System.out.println(1000 + 2000);
+        System.out.println(3000 - 1500);
+        System.out.println(50 * 40);
+        System.out.println(600 / 15);
+        System.out.println(5 % 2);
+
+	}
+
+}
