fea0d30819b0b94a80effe2afc7de3a838767db8
kenshi39847 <kenshi.t.blue@gmail.com>
2023-12-01 13:51:20 +0900

    条件分岐の練習

---
+++ src/to/msn/wings/selflearn/chap04/Chap04Practice.java
@@ -0,0 +1,18 @@
+package to.msn.wings.selflearn.chap04;
+
+public class Chap04Practice {
+
+	public static void main(String[] args) {
+		int value = 3;
+
+	    if (value > 0){
+	      System.out.println("値は正です"); 
+	    }else if (value < 0){
+	      System.out.println("値は負です"); 
+	    }else {
+	      System.out.println("値は0です"); 
+	    }
+
+	}
+
+}
