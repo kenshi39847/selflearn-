f1d3300f825bbfaf9ed34708ab4ba4dfcc791faf
kenshi39847 <kenshi.t.blue@gmail.com>
2023-12-02 18:04:41 +0900

    文字列が正規表現パターンにマッチしたかを判定する

--- src/to/msn/wings/selflearn/chap05/RegularExpression.java
+++ src/to/msn/wings/selflearn/chap05/RegularExpression.java
@@ -1,9 +1,16 @@
 package to.msn.wings.selflearn.chap05;
 
+import java.util.regex.Pattern;
+
 public class RegularExpression {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		var tel = new String[] { "080-0000-0000", "084-000-0000", "184-0000" };
+	    var rx = "\\d{2,4}-\\d{2,4}-\\d{4}";
+	    for (var t : tel) {
+	      System.out.println(Pattern.matches(rx, t) ? t : "アンマッチ");
+	      // System.out.println(t.matches(rx) ? t : "アンマッチ");
+	    }
 
 	}
 
@@ -22,4 +29,16 @@ public class RegularExpression {
   [^XYZ] |X,Y,Z以外のいずれかの1文字
   [X-Z]  |X～Zの範囲の中の1文字
   [X|Y|Z]|X,Y,Zのいずれか
+*/
+
+/*
+  正規表現パターンが文字列にマッチしているかを判定するには、「Patternクラス(Java.util.regexパッケージ)のmatches静的メソッド」を利用する。
+  パターンが文字列全体に一致（=完全一致）する場合だけtrueを返します。
+*/
+
+/*
+  matchesメソッド
+  public static boolean matches(String regex, CharSequence input)
+  regex:正規表現
+  input:検索する文字列
 */
