6c53b7078c3efec60c89d214659f9a6f107e3e59
kenshi39847 <kenshi.t.blue@gmail.com>
2023-12-04 14:50:59 +0900

    正規表現による検索

--- src/to/msn/wings/selflearn/chap05/RegularExpression.java
+++ src/to/msn/wings/selflearn/chap05/RegularExpression.java
@@ -1,6 +1,16 @@
 package to.msn.wings.selflearn.chap05;
 
+import java.util.regex.Pattern;
+
 public class RegularExpression {
+	
+	private static void match(Pattern ptn, String input) {
+	    var match = ptn.matcher(input);
+	    while (match.find()) {
+	      System.out.println(match.group());
+	    }
+	    System.out.println("---");
+	  }
 
 	public static void main(String[] args) {
 		/*
@@ -62,6 +72,69 @@ public class RegularExpression {
         //      よろしくお願いします。
 	    */
 	    
+		/*
+        var tags = "<p><strong>WINGS</strong>サイト<a href='index.html'><img src='wings.jpg' /></a></p>";
+        //var ptn = Pattern.compile("<.+>");
+        var ptn = Pattern.compile("<.+?>");
+        var match = ptn.matcher(tags);
+        while (match.find()) {
+          System.out.println(match.group());
+        }
+        */
+		
+		/*
+		var msg = "会社の電話は0123-99-0000です。自宅は000-123-4567だよ。";
+	    var ptn = Pattern.compile("(?<area>\\d{2,4})-(?<city>\\d{2,4})-(?<local>\\d{4})");
+	    var match = ptn.matcher(msg);
+	    while (match.find()) {
+	      System.out.println("開始位置：" + match.start());
+	      System.out.println("終了位置：" + match.end());
+	      System.out.println("マッチング文字列：" + match.group());
+	      System.out.println("市外局番：" + match.group("area"));
+	      System.out.println("市内局番：" + match.group("city"));
+	      System.out.println("加入者番号：" + match.group("local"));
+	      System.out.println("-----");
+	    }
+	    */
+	    
+		/*
+	    var str = "<p>サポートサイト<a href=\"https://www.wings.msn.to/\">https://www.wings.msn.to/</a></p>";
+	    var ptn = Pattern.compile("<a href=\"(.+?)\">\\1</a>");
+	    // var ptn = Pattern.compile("<a href=\"(?<link>.+?)\">\\k<link></a>");
+	    var match = ptn.matcher(str);
+	    if (match.find()) {
+	      System.out.println(match.group());  //結果：<a href="https://www.wings.msn.to/">https://www.wings.msn.to/</a>
+	    }
+	    */
+		
+		/*
+		var str = "仕事用はwings@example.comです。プライベート用はYAMA@example.comです。";
+	    var ptn = Pattern.compile("([a-z0-9.!#$%&'*+/=?^_{|}~-]+)@([a-z0-9-]+(\\.[a-z0-9-]+)*)", Pattern.CASE_INSENSITIVE);
+	    // var ptn = Pattern.compile("([a-z0-9.!#$%&'*+/=?^_{|}~-]+)@([a-z0-9-]+(?:\\.[a-z0-9-]+)*)", Pattern.CASE_INSENSITIVE);
+	    var match = ptn.matcher(str);
+	    while (match.find()) {
+	      System.out.println(match.group());
+	      System.out.println(match.group(1));
+	      System.out.println(match.group(2));
+	      System.out.println(match.group(3));
+	      System.out.println("-----");
+	    }
+	    */
+		
+		var re1 = Pattern.compile("いろ(?=はに)");
+	    var re2 = Pattern.compile("いろ(?!はに)");
+	    var re3 = Pattern.compile("(?<=。)いろ");
+	    var re4 = Pattern.compile("(?<!。)いろ");
+	    var msg1 = "いろはにほへと";
+	    var msg2 = "いろものですね。いろいろと";
+	    match(re1, msg1);
+	    match(re1, msg2);
+	    match(re2, msg1);
+	    match(re2, msg2);
+	    match(re3, msg1);
+	    match(re3, msg2);
+	    match(re4, msg1);
+	    match(re4, msg2);
 
 	}
 
@@ -142,4 +215,38 @@ public class RegularExpression {
   UNIX_LINES      |「\n」だけを行末記号として扱う
   LITERAL         |パターンをリテラル文字として解析（「\d」などの意味を無効化）
   COMMENTS        |空白とコメントを有効化
+*/
+
+/*
+  正規表現で「*」、「+」などの量指定子を利用した場合、できるだけ長い文字列を一致させるというルールを「最長一致」という。
+  また、できるだけ短い文字列を一致させるというルールを「最短一致」という。「+?」を使う。
+*/
+
+/*
+  正規表現パターンに含まれる(...)でくくられた部分パターンのことを「グループ（キャプチャグループ）」という。
+  グループには意味のある名前を付与することができ、これを「名前付きキャプチャグループ」という。
+  var ptn = Pattern.compile("(?<area>\\d{2,4})-(?<city>\\d{2,4})-(?<local>\\d{4})");
+  名前はグループの先頭で「?<...>」の形式で宣言するだけである。
+  名前付きキャプチャグループにアクセスするには、groupメソッドにも文字列を渡す。
+*/
+
+/*
+  グループにマッチした文字列は正規表現パターンの中であとから参照することもできる（「後方参照」）。
+  一般的なグループは「\1」のような番号で後方参照できる。
+  var ptn = Pattern.compile("<a href=\"(.+?)\">\\1</a>");
+  複数グループがある場合は、\2、\3...のように指定する。
+*/
+
+/*
+  var ptn = Pattern.compile("([a-z0-9.!#$%&'*+/=?^_{|}~-]+)@([a-z0-9-]+(\\.[a-z0-9-]+)*)", Pattern.CASE_INSENSITIVE);
+  上記コードでは正規表現パターンに3個のグループが含まれている。
+*/
+
+/*
+  後読みと先読み
+  表現    |概要
+  A(?=B)  |肯定的先読み（Aの緒後にBが続く場合にだけAにマッチ）
+  A(?!B)  |否定的先読み（Aの直後にBが続かない場合だけAにマッチ）
+  (?<=B)A |肯定的後読み（Aの直前にBがある場合にだけAにマッチ）
+  (?<!B)A |否定的後読み（Aの直前にBがない場合にだけAにマッチ）
 */
