a40b0be582998832e2f8818d70baa444cc7c614b
kenshi39847 <kenshi.t.blue@gmail.com>
2023-12-07 14:53:34 +0900

    コーディング上のイディオム（コレクションの初期化・コレクションを順に処理する）

--- src/to/msn/wings/selflearn/chap06/CollectionsFramework.java
+++ src/to/msn/wings/selflearn/chap06/CollectionsFramework.java
@@ -1,9 +1,21 @@
 package to.msn.wings.selflearn.chap06;
 
+import java.util.ArrayList;
+
 public class CollectionsFramework {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		
+		var data = new ArrayList<String>() {  //・・・匿名クラス
+		  {                                   //・・・初期化ブロック
+		    add("バラ");
+		    add("ひまわり");
+		    add("あさがお");
+		  }
+		};
+		for (var s : data) {
+		  System.out.println(s);
+		}
 
 	}
 
@@ -40,4 +52,47 @@ public class CollectionsFramework {
   コレクションは、一般的に上記の構文でインスタンス化する。
   例えば、ArrayListクラスをインスタンス化するには、以下のようなコードになる。
   List<String> data = new ArrayList<>();
+*/
+
+/*
+  コレクション配下の要素を順番に処理するには、拡張for命令を利用する。
+  この拡張for命令は内部的にはイテレーターを利用したwhile命令のシンタックスシュガー（より簡単化された構文）です。
+  「イテレーター」とは、コレクションの要素を順番に取り出すための仕組みである。
+  
+  コレクションからはiteratorメソッドを利用することで、それぞれの実装に応じたIteratorオブジェクト（イテレーター）を取得できる。
+  以下のコードはイテレーターを使ったものである。
+  var itr = data.iterator();
+  while (itr.hasNext()) {
+    System.out.println(itr.next());
+  }
+  
+  拡張forの方がコードをシンプルに表現できることから、一般的にはイテレーターを直接利用することはほとんどない。
+  しかし、イテレーターを利用しなければならない場面もある。
+  1.ループの中で要素を削除したい
+  var data = new ArrayList<String>() {
+    {
+      add("バラ");
+      add("ひまわり");
+      add("あさがお");
+    }
+  };
+  var ite = data.iterator();
+  while (ite.hasNext()) {
+    System.out.println(ite.next());
+    ite.remove();
+  }
+  System.out.println(data);
+  
+  2.リストを逆順に読み込む
+  var data = new ArrayList<String>() {
+    {
+      add("バラ");
+      add("ひまわり");
+      add("あさがお");
+    }
+  };
+  var ite = data.listIterator(data.size());
+  while (ite.hasPrevious()) {
+    System.out.println(ite.previous());
+  }
 */
