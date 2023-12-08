1cc5373e21e712b96a8820dbf513e332a510e23f
kenshi39847 <kenshi.t.blue@gmail.com>
2023-12-09 08:16:26 +0900

    Practice4-2

---
+++ src/to/msn/wings/selflearn/chap04/Chap04Practice2_1.java
@@ -0,0 +1,11 @@
+package to.msn.wings.selflearn.chap04;
+
+public class Chap04Practice2_1 {
+
+	public static void main(String[] args) {
+	/* [1] while命令とdo...while命令との違いを説明してください */
+	/* while命令はループの先頭で条件式を判定するが、while...do命令はループの末尾で条件式を判定する。 */
+
+	}
+
+}
