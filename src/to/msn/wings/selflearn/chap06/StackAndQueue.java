1eac40a283d2c0e801efbf4d3d2489a6b971946a
kenshi39847 <kenshi.t.blue@gmail.com>
2023-12-08 11:36:13 +0900

    スタック/キュー

---
+++ src/to/msn/wings/selflearn/chap06/StackAndQueue.java
@@ -0,0 +1,20 @@
+package to.msn.wings.selflearn.chap06;
+
+public class StackAndQueue {
+
+  public static void main(String[] args) {
+	
+
+
+  }
+
+}
+
+/*
+  リスト両端からの値の出し入れに特化したデータ構造が「スタック」と「キュー」である。
+  「スタック（Stack）」とは、後入れ先だし（LIFO:Last In First Out）、または先入れ後出し（FILO:First In Last Out）とも呼ばれるデータ構造のことである。
+  「キュー（Queue）」は先入れ先出し（FIFO:First In first Out）と呼ばれるデータ構造のことである。待ち行列とも呼ばれる。
+  Javaではこれらのデータ構造を実現するために「デック（Deque）」というインターフェースを提供している。
+  「デック（Deque）」は両端キューと呼ばれ、リストの先頭/末尾双方から要素を追加/削除できる構造である。
+  スタック/キューの主なクラスにはArrayDeque、LinkedList、Stackがある。
+*/
