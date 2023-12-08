f39cd77bcc0f6541c2f900b5e2c985d1d008f4df
kenshi39847 <kenshi.t.blue@gmail.com>
2023-12-08 16:59:04 +0900

    配列とリストの練習

---
+++ src/to/msn/wings/selflearn/chap06/Chap06Practice.java
@@ -0,0 +1,22 @@
+package to.msn.wings.selflearn.chap06;
+
+import java.util.ArrayList;
+
+public class Chap06Practice {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> scores = new ArrayList<Integer>();
+
+	    scores.add(1);
+	    scores.add(5);
+	    scores.add(10);
+	    scores.add(15);
+
+	    System.out.println(scores.get(0));
+	    System.out.println(scores.get(1));
+	    System.out.println(scores.get(2));
+	    System.out.println(scores.get(3));
+
+	}
+
+}
