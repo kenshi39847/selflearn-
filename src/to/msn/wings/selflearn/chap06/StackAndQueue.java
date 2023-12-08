5c2cbe843520e1084a3c0da6bdcd8d11fcad9f69
kenshi39847 <kenshi.t.blue@gmail.com>
2023-12-08 12:21:32 +0900

    ArrayDeque

--- src/to/msn/wings/selflearn/chap06/StackAndQueue.java
+++ src/to/msn/wings/selflearn/chap06/StackAndQueue.java
@@ -1,10 +1,29 @@
 package to.msn.wings.selflearn.chap06;
 
+import java.util.ArrayDeque;
+
 public class StackAndQueue {
 
   public static void main(String[] args) {
-	
+	//スタック（末尾から要素を追加し、取り出す）
+	var data = new ArrayDeque<Integer>();
+	  data.addLast(10);
+	  data.addLast(15);
+	  data.addLast(30);
 
+	  System.out.println(data);                 //結果：[10, 15, 30]
+	  System.out.println(data.removeLast());    //結果：30
+	  System.out.println(data);                 //結果：[10, 15]
+	  
+      //キュー（末尾から要素を追加し、先頭から取り出す）
+	  var data2 = new ArrayDeque<Integer>();
+	  data2.addLast(10);
+	  data2.addLast(15);
+	  data2.addLast(30);
+
+	  System.out.println(data2);                //結果：[10, 15, 30]
+	  System.out.println(data2.removeFirst());  //結果：10
+	  System.out.println(data2);                //結果：[15, 30]
 
   }
 
@@ -17,4 +36,9 @@ public class StackAndQueue {
   Javaではこれらのデータ構造を実現するために「デック（Deque）」というインターフェースを提供している。
   「デック（Deque）」は両端キューと呼ばれ、リストの先頭/末尾双方から要素を追加/削除できる構造である。
   スタック/キューの主なクラスにはArrayDeque、LinkedList、Stackがある。
+*/
+
+/*
+  ArrayDequeの内部的な実装は循環配列である。
+  循環配列とは、基本的には配列だが、先頭から順に要素を格納するのではなく、配列内の任意の範囲に要素を格納している。
 */
