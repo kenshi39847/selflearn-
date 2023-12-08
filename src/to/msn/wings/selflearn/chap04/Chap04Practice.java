e9824f8bb709f1bedd9393761df8f86c9182c732
kenshi39847 <kenshi.t.blue@gmail.com>
2023-12-08 17:03:03 +0900

    繰り返し処理の練習

--- src/to/msn/wings/selflearn/chap04/Chap04Practice.java
+++ src/to/msn/wings/selflearn/chap04/Chap04Practice.java
@@ -1,8 +1,11 @@
 package to.msn.wings.selflearn.chap04;
 
+import java.util.ArrayList;
+
 public class Chap04Practice {
 
 	public static void main(String[] args) {
+		/*
 		int value = 3;
 
 	    if (value > 0){
@@ -11,6 +14,18 @@ public class Chap04Practice {
 	      System.out.println("値は負です"); 
 	    }else {
 	      System.out.println("値は0です"); 
+	    }
+	    */
+		
+		ArrayList<Integer> scores = new ArrayList<Integer>();
+
+	    scores.add(1);
+	    scores.add(5);
+	    scores.add(10);
+	    scores.add(15);
+
+	    for(int score : scores) {
+	      System.out.println(score);  
 	    }
 
 	}
