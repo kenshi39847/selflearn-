1c4e436f66cf0a87cb59b614ab0efd0efd795d8f
kenshi39847 <kenshi.t.blue@gmail.com>
2023-12-12 11:03:38 +0900

    厳密な変数の有効範囲

--- src/to/msn/wings/selflearn/chap07/scope/Scope.java
+++ src/to/msn/wings/selflearn/chap07/scope/Scope.java
@@ -3,9 +3,14 @@ package to.msn.wings.selflearn.chap07.scope;
 public class Scope {
 
 	public static void main(String[] args) {
+		/*
 		var s = new ScopeBasic();
 		System.out.println(s.show());
 		System.out.println(s.date);
+		*/
+		
+		var s = new ScopeStrict();
+	    s.show();
 
 	}
 
@@ -19,4 +24,19 @@ public class Scope {
 最も有効範囲が狭い変数は、メソッド配下のブロックで宣言された変数で、具体的には、
 if、while/forなどで制御ブロックの配下で宣言された変数と言い換えてもよい。
 このような変数を便宜的に「ブロック変数」といい、ブロック配下でのみアクセスできる。
+*/
+
+/*
+変数の有効範囲を正確に表現するならば「宣言された位置からブロックの終端までが有効範囲」となる。
+public class ScopeStrict {
+  String str1 = "いろはにほへと";
+  String str2 = str1;
+}
+上記のコードは正しいコードであるが、以下のコード（2,3行目を逆にしたもの）は不可である。
+public class ScopeStrict {
+  String str2 = str1;
+  String str1 = "いろはにほへと";
+}
+str1を参照している時点で、str1はまだ宣言されていないからであり、これが変数が「宣言された位置から有効」という意味である。
+ただし、メソッド/コンストラクターからの参照は例外で、フィールドの宣言開始位置に関わらず、どこからでも参照できる。
 */
---
+++ src/to/msn/wings/selflearn/chap07/scope/ScopeStrict.java
@@ -0,0 +1,8 @@
+package to.msn.wings.selflearn.chap07.scope;
+
+public class ScopeStrict {
+	public void show() {
+	    System.out.println(str);
+	}
+	String str = "いろはにほへと";
+}
