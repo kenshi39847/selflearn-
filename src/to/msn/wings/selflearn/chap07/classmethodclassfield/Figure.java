7ef3a5c3f2d2f5cf35eff4e298178bd402deed35
kenshi39847 <kenshi.t.blue@gmail.com>
2023-12-13 12:14:33 +0900

    クラスメソッド

---
+++ src/to/msn/wings/selflearn/chap07/classmethodclassfield/Figure.java
@@ -0,0 +1,7 @@
+package to.msn.wings.selflearn.chap07.classmethodclassfield;
+
+public class Figure {
+	public static double getTriangleArea(double width, double height) {
+	    return width * height / 2;
+	}
+}
