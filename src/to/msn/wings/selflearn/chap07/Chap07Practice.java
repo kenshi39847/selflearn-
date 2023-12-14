68881aeb170077ef9d3a0264c611a91f94bf9397
kenshi39847 <kenshi.t.blue@gmail.com>
2023-12-14 12:23:54 +0900

    メソッドの練習

---
+++ src/to/msn/wings/selflearn/chap07/Chap07Practice.java
@@ -0,0 +1,20 @@
+package to.msn.wings.selflearn.chap07;
+
+public class Chap07Practice {
+
+	public static void main(String[] args) {
+	/*
+		sayHello();
+	}
+	public static void sayHello() {
+	    System.out.println("Hello World");
+	    return;
+	*/
+		var answer = square(5);
+	    System.out.println(answer);
+	}
+	public static int square(int number){
+	    return number * number;
+	}
+
+}
