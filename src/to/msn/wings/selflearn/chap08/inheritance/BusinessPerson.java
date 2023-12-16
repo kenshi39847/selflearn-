46cff1770390fe0af0972a4daf52e47a95bbaa04
kenshi39847 <kenshi.t.blue@gmail.com>
2023-12-16 09:05:08 +0900

    メソッドのオーバーライド

--- src/to/msn/wings/selflearn/chap08/inheritance/BusinessPerson.java
+++ src/to/msn/wings/selflearn/chap08/inheritance/BusinessPerson.java
@@ -14,5 +14,16 @@ public class BusinessPerson extends Person {
 	    System.out.println(super.birth);
 	}
 	*/
+	
+	/*オーバーライド
+	public BusinessPerson() {}
+	@Override 
+	public String show() {
+	    return String.format("会社員の%s（%d歳）です。", this.name, this.age);
+	}
+	public String work() {
+	    return String.format("%d歳の%sは、働きます。", this.age, this.name);
+	}
+	*/
 
 }
