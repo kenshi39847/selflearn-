62468a16a2b184339b7699d162dca34e4fb3b517
kenshi39847 <kenshi.t.blue@gmail.com>
2023-12-29 10:19:58 +0900

    Practice5-4

--- src/to/msn/wings/selflearn/chap05/Chap05Practice4_4.java
+++ src/to/msn/wings/selflearn/chap05/Chap05Practice4_4.java
@@ -4,11 +4,20 @@
        3.110,14,28,32といった値を持つ配列を定義し、これをソートする */
 package to.msn.wings.selflearn.chap05;
 
+import java.util.Arrays;
+
 public class Chap05Practice4_4 {
 
 	public static void main(String[] args) {
 		
-
+		//1.
+		System.out.println(Math.pow(6, 3));
+		//2.
+		System.out.println(Math.abs(-100));
+		//3.
+		var array = new int[] {110, 14, 28, 32};
+		Arrays.sort(array);
+		System.out.println(Arrays.toString(array));
 	}
 
 }
